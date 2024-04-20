31210474177881aaf540f48fbc1df60ec802c847
tdamrob <damir.robic@ericssonnikolatesla.com>
2024-04-20 10:43:25 +0200

    prvi komit

---
+++ PrazniProjektNaGit/src/hr/edu/NeRadiNista.java
@@ -0,0 +1,11 @@
+package hr.edu;
+
+public class NeRadiNista {
+
+	public static void main(String[] args) {
+		System.out.println("Ne radi ništa   ");
+		System.out.println("Dodajem promjenu");
+
+	}
+
+}
---
+++ PrazniProjektNaGit/src/hr/edu/NiOvaNeRadiNista.java
@@ -0,0 +1,5 @@
+package hr.edu;
+
+public class NiOvaNeRadiNista {
+
+}
